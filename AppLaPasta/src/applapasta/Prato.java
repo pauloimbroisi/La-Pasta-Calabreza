979787bb539316111fa7c6b8c365703705315cf4
pauloimbroisi <pauloj@inatel.br>
2020-11-26 18:41:16 -0300

    Criando a Classe 'Prato'

---
+++ AppLaPasta/src/applapasta/Prato.java
@@ -0,0 +1,38 @@
+package applapasta;
+
+public class Prato {
+    
+    private String nome;
+    private float preco;
+    private int numPessoasServidas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getNumPessoasServidas() {
+        return numPessoasServidas;
+    }
+
+    public void setNumPessoasServidas(int numPessoasServidas) {
+        this.numPessoasServidas = numPessoasServidas;
+    }
+    
+    public void mostrarInfo(){
+        System.out.println("Nome do prato:" + this.nome +
+                "\nPreço: R$" + this.preco + 
+                "\nServe " + this.numPessoasServidas + " pessoas.\n");
+    }
+}
